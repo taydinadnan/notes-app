295f832e386762df76bdf32614bddc6455989ff4
Adnan Turgay Aydin <taydinadnan@gmail.com>
2023-10-26 20:12:35 +0200

    Resolve packages

--- lib/provider/firebase_authentication.dart
+++ lib/provider/firebase_authentication.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes_app/repository/user_data_repository.dart';
-import 'package:riverpod/riverpod.dart';
 
 final authProvider = Provider((ref) {
   return FirebaseAuth.instance;
